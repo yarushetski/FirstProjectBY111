960ba84267b2a1aed784997dca5603ec9c7ae4a1
yarushetski <107986895+yarushetski@users.noreply.github.com>
2022-06-22 11:58:35 +0300

    add pause

---
+++ TemplateGit/TemplateGit/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << " привет " << endl;
+
+
+
+	system("pause");
+	return 0;
+}
