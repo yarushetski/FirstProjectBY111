873369b2896621da4f3035fb2d8bfba90e3fc695
yarushetski <107986895+yarushetski@users.noreply.github.com>
2022-06-22 12:53:35 +0300

    add typedef ull

--- TemplateGit/TemplateGit/Source.cpp
+++ TemplateGit/TemplateGit/Source.cpp
@@ -3,8 +3,25 @@
 
 using namespace std;
 
+using d_t = double; //псевдоним типов 1 sposob
+typedef unsigned long long ull_t; //псевдоним типов  2 sposob
+
+unsigned long long i = 991;
+ull_t i2 = 991;
+d_t i3 = 99.9;
+
+ull_t add(ull_t a, ull_t b)
+{
+	return a + b;
+}
+
 int main()
 {
+	for (size_t i = 0; i < 5; i++)
+	{
+
+	}
+
 	setlocale(LC_ALL, "");
 	cout << " привет " << endl;
 
