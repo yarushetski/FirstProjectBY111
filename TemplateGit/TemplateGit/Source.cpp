154d74d8258ab8d0a98530a249ab97f2ec15e1b9
yarushetski <107986895+yarushetski@users.noreply.github.com>
2022-06-23 13:43:45 +0300

    11

--- TemplateGit/TemplateGit/Source.cpp
+++ TemplateGit/TemplateGit/Source.cpp
@@ -1,3 +1,7 @@
+
+
+
+
 #include <iostream>
 #include <string>
 
@@ -10,13 +14,24 @@ unsigned long long i = 991;
 ull_t i2 = 991;
 d_t i3 = 99.9;
 
+/// <summary>
+/// сложение двух елых чисел 
+/// </summary>
+/// <param name="a"> первое число </param>
+/// <param name="b"> второе число </param>
+/// <returns> а + b </returns>
+
 ull_t add(ull_t a, ull_t b)
 {
 	return a + b;
 }
 
+
+
 int main()
 {
+	add
+
 	for (size_t i = 0; i < 5; i++)
 	{
 
